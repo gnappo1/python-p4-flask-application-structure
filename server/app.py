5d063d99705b16f6a350fad1d8fa7c7566c1fe7c
Matteo Piccini <gnappo.1@live.it>
2023-03-29 14:51:56 -0700

    done.

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route("/")
+def index():
+    return "<h1>Sup Gente!</h1>"
+
+@app.route("/<string:username>")
+def show(username):
+    return f"<h1>Sup {username}!</h1>"
+
+
+# from command line
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
